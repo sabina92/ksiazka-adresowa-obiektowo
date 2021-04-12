667adcc97659c01ea42ee621cc70ec015bbcefef
Sabina Gorni <sabina.ksiazek.programista@gmail.com>
2021-04-12 22:57:01 +0200

    Add registered user to vector

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "Uzytkownik.h"
+#include <windows.h>
 
 using namespace std;
 
@@ -12,6 +13,11 @@ class KsiazkaAdresowa
 
     vector <Uzytkownik> uzytkownicy;
 
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+
 public:
     void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
 };
